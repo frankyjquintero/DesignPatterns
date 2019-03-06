2486c9961a2c88fc0fc0de841749c971447740f5
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-03-06 14:59:46 -0500

    Observer

---
+++ DesignPatterns.Observer/Observador.cs
@@ -0,0 +1,4 @@
+public interface Observador
+{
+    void actualiza();
+}
---
+++ DesignPatterns.Observer/Vehiculo.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Vehiculo : Sujeto
+{
+    protected string _descripcion;
+    protected double _precio;
+
+    public string descripcion
+    {
+        get
+        {
+            return _descripcion;
+        }
+        set
+        {
+            _descripcion = value;
+            this.notifica();
+        }
+    }
+
+    public double precio
+    {
+        get
+        {
+            return _precio;
+        }
+        set
+        {
+            _precio = value;
+            this.notifica();
+        }
+    }
+
+}
